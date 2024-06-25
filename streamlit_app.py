aba8ba6fbae74fc94302f306686ea3faef04f8c0
avneetreen <avneet14027@iiitd.ac.in>
2024-06-25 17:29:39 +0200

    feat: demo app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import requests
+import pandas as pd
+from ast import literal_eval
+import re
 
-st.header("Humanity-deploy", divider='rainbow')
-st.subheader("War & Peace")
-st.write("This is a simple Streamlit app.")
+st.header("War & Peace", divider='rainbow')
+st.subheader(
+    "Editorial Pattens in News Coverage through the lens of War & Peace Journalism")
+# st.write("This is a simple Streamlit app.")
 
 
 # Function to fetch page data from FastAPI
@@ -18,17 +22,58 @@ def fetch_page(page: str) -> dict:
 
 # Streamlit sidebar navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "About", "Contact"])
+page = st.sidebar.radio("Go to", ["Home"])
 
 # Fetch page data
 page_data = fetch_page(page.lower())
 
+
+def highlight_text(text, highlights):
+    """Highlight the specified words/phrases in the content."""
+    for highlight in highlights:
+        print(highlight)
+        escaped_highlight = re.escape(highlight)  # Escape special characters
+        pattern = re.compile(f"({escaped_highlight})", re.IGNORECASE)
+        text = pattern.sub(
+            r'<mark style="background-color: lightpink;">\1</mark>', text)
+    return text
+
+
 # Render the appropriate page using Streamlit elements
 if page_data["page"] == "home":
-    st.title("Home Page")
-    st.write(
-        "Welcome to the Home Page! This is a dynamic page created using Streamlit.")
-    st.line_chart([1, 2, 3, 4, 5])
+    st.title("Visible Effects of War")
+
+    # df = pd.DataFrame(data)
+    # Create a DataFrame
+    df = pd.read_csv("data/some_results.csv")
+
+    # Streamlit application
+    st.subheader("Article Viewer")
+
+    # Dropdown for selecting an article
+    article_title = st.selectbox(
+        "Select and article to highlight keywords and identify FrameNet Frames associated with visible effects of war",
+        df["title"])
+
+    # Get the selected article's details
+    selected_article = df[df["title"] == article_title].iloc[0]
+    print(literal_eval(selected_article["all_text_to_highlight"]),
+          selected_article["maintext"])
+
+    # Highlight the text
+    highlighted_content = highlight_text(
+        selected_article["maintext"],
+        literal_eval(selected_article["all_text_to_highlight"]))
+
+    # print(highlighted_content)
+
+    # Display the highlighted article content
+    st.markdown(highlighted_content, unsafe_allow_html=True)
+
+    # Display the keywords in the sidebar
+    st.sidebar.title("FrameNet Frames")
+    for keyword in set(literal_eval(selected_article["all_frames"])):
+        st.sidebar.write(keyword)
 
 elif page_data["page"] == "about":
     st.title("About Page")
